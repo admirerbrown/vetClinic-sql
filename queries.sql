b3bd1235a7086acd8619083f4cb66efde5fa0893
admirerbrown <admirerbrown@gmail.com>
2023-02-23 19:25:43 +0000

    day3 queries on the vet_clinic SQL database

--- queries.sql
+++ queries.sql
@@ -41,3 +41,56 @@ SELECT name, escape_attempts FROM animals where escape_attempts = (SELECT MAX(es
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT name, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY name;
+
+-- Remove column species
+ALTER TABLE animals DROP COLUMN species;
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD species_id BIGINT;
+ALTER TABLE animals
+ADD CONSTRAINT FK_species
+FOREIGN KEY (species_id) REFERENCES species(id) 
+-- Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+-- All other animals are Pokemon
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD owner_id BIGINT;
+ALTER TABLE animals
+ADD CONSTRAINT FK_owner
+FOREIGN KEY (owner_id) REFERENCES owners(id); 
+
+-- Sam Smith owns Agumon.
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+-- Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+-- Bob owns Devimon and Plantmon.
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+-- Dean Winchester owns Angemon and Boarmon.
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+
+-- What animals belong to Melody Pond?
+SELECT name, full_name FROM animals a JOIN owners o ON o.id = a.owner_id WHERE o.full_name = 'Melody Pond';
+-- List of all animals that are pokemon (their type is Pokemon).
+SELECT a.name, s.name FROM animals a JOIN species s ON s.id = a.species_id WHERE s.name = 'Pokemon';
+-- List all owners and their animals, remember to include those that don't own any animal.
+SELECT name, full_name FROM animals a JOIN owners o ON o.id = a.owner_id 
+UNION ALL SELECT name, full_name FROM owners o LEFT JOIN animals a ON o.id = a.owner_id WHERE a.name IS NULL;
+-- How many animals are there per species?
+SELECT s.name, count(*) FROM animals a JOIN species s ON s.id = a.species_id GROUP BY s.name;
+-- List all Digimon owned by Jennifer Orwell.
+SELECT name, species_id FROM animals a JOIN owners o ON o.id = a.owner_id WHERE (SELECT o.full_name = 'Jennifer Orwell' WHERE a.species_id = 2);
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT name, escape_attempts, full_name FROM animals a JOIN owners o ON o.id = a.owner_id WHERE (SELECT o.full_name = 'Dean Winchester' WHERE a.escape_attempts = 0);
+-- Who owns the most animals?
+SELECT a.owner_id, o.full_name, COUNT(a.owner_id) AS appeared 
+FROM animals a 
+JOIN owners o ON o.id = a.owner_id 
+GROUP BY a.owner_id, o.full_name 
+ORDER BY appeared DESC 
+LIMIT 1;
+
+
